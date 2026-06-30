1c2b1ac47c22c07f52012dca66a7861fc6c1fa81
paul <ecscorpresources@gmail.com>
2026-06-30 17:22:48 +0100

    new onboarding

--- src/pages/signup/ServiceProvider/schema.js
+++ src/pages/signup/ServiceProvider/schema.js
@@ -1,29 +1,30 @@
-import * as Yup from 'yup'
+import * as Yup from "yup";
 
 export const SignUpSchema = Yup.object().shape({
-  email: Yup.string().email('Invalid email address').required('Email is required'),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
   password: Yup.string()
-    .min(8, 'Must be 8 characters or more')
-    .max(20, 'Must not exceed 20 characters')
-    .required('Password is required'),
-//   term: Yup.boolean().oneOf([true], 'This field is required'),
+    .min(8, "Must be 8 characters or more")
+    .max(20, "Must not exceed 20 characters")
+    .required("Password is required"),
+  //   term: Yup.boolean().oneOf([true], 'This field is required'),
   phoneNumber: Yup.string()
     .matches(/^\d+$/, "Phone number must contain digits only")
     .length(11, "Phone number must be 11 digits")
     .required("Please enter your phone number"),
   fullName: Yup.string()
-    .required('Please enter your first name and last name')
+    .required("Please enter your first name and last name")
     .test(
-      'first-last-name',
-      'Please enter your first name and last name',
+      "first-last-name",
+      "Please enter your first name and last name",
       (value) => (value ? value.trim().split(/\s+/).length >= 2 : false),
     ),
-
-
 });
 
 export const PersonalInfoSchema = Yup.object().shape({
-   gender: Yup.string().required('Please select your gender'),
-   city: Yup.string().required('Please enter your city'),
-   address: Yup.string().required('Please enter your address')
+  gender: Yup.string().required("Please select your gender"),
+  city: Yup.string().required("Please enter your city"),
+  address: Yup.string().required("Please enter your address"),
+  ninSlip: Yup.string().required("Please upload your nin slip"),
 });
